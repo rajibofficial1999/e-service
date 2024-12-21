46cc46a2f448df0d0cd7165cd3f80940681d4496
MD Raib <rajibofficial1999@gmail.com>
2024-12-21 17:05:44 +0600

    install shadcn and country, states, city model, and controller

---
+++ app/Models/State.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class State extends Model
+{
+    /** @use HasFactory<\Database\Factories\StateFactory> */
+    use HasFactory;
+
+    protected $fillable = ['country_id','name', 'slug', 'state_code', 'active'];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+}
---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/Components/ui/button";
+
+const Home = () => {
+  return (
+      <>
+          <h1>Home</h1>
+          <Button>Click Me</Button>
+      </>
+  )
+}
+
+export default Home;
